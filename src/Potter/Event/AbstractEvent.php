eb17dff168ea405b276f1203e31ceba92691fafb
Jay Potter <j@ypotter.ca>
2024-03-31 12:46:03 -0400

    emitter

--- src/Potter/Event/AbstractEvent.php
+++ src/Potter/Event/AbstractEvent.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Potter\Event;
 
+use \Potter\Event\Emitter\EmitterInterface;
+
 abstract class AbstractEvent implements EventInterface
 {
+    abstract public function getEmitter(): EmitterInterface;
+    abstract protected function setEmitter(EmitterInterface $emitter): void;
     abstract public function getId(): string;
     abstract protected function setId(string $id): void;
 }
--- src/Potter/Event/EventInterface.php
+++ src/Potter/Event/EventInterface.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Potter\Event;
 
+use \Potter\Event\Emitter\EmitterInterface;
+
 interface EventInterface
 {
+    public function getEmitter(): EmitterInterface;
     public function getId(): string;
 }
